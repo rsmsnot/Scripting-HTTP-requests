8f4f8fb9f6227b089d80ecd75f172b2cae596a4a
rsmsnot <rsmsnot@outlook.com>
2019-06-03 18:40:30 +0000

    step-1

---
+++ printing-in-chunks.js
@@ -0,0 +1,23 @@
+function getAndPrintHTMLChunks() {
+
+    var https = require('https');
+
+    var requestOptions = {
+        host: 'sytantris.github.io',
+        path: '/http-examples/step1.html'
+    };
+
+    https.get(requestOptions, function(response) {
+
+        var buffer = ""
+        response.setEncoding('utf8');
+
+        response.on('data', function(data) {
+            buffer += data;
+            console.log(buffer)
+
+        });
+    });
+}
+
+getAndPrintHTMLChunks();
